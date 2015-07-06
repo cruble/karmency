2976369085603111aabdd1fc5d10d07be40e0729
cruble <cruble4@gmail.com>
2015-07-06 11:49:02 -0400

    coin tests complete and passed

--- spec/models/coin_spec.rb
+++ spec/models/coin_spec.rb
@@ -2,54 +2,57 @@ require 'rails_helper'
 
 describe Coin, type: :model do
 
-  let(:creator) { create(:user) }
-  let(:coin) { create(:coin, creator_id: creator.id) }
-  let(:users) { create_list(:user, 4) }
-  let(:moments) { create_list(:moment, 4) }
+  before(:all) do
+    @creator = create(:user)
+    @coin = create(:coin, creator_id: @creator.id)
+    @users = create_list(:user, 4)
+    @moments = create_list(:moment, 4)
 
-  before(:each) do
-    # associate moments with coin
-    moments.each do |moment|
-      coin.moments << moment
-    end
-    # associate users with moments
-    moments.each_with_index do |moment, index|
+    # for each moment add a giver and receiver to a coin
+
+    @moments.each_with_index do |moment, index|
       if index == 0
-        coin.givers << creator
+        moment.giver = @creator
       else
-        coin.givers << users[index - 1]
+        moment.giver = @users[index - 1]
       end
-      coin.receivers << users[index]
+
+      moment.receiver = @users[index]
     end
+
+    # associate moments with coin
+    @moments.each do |moment|
+      @coin.moments << moment
+    end
+
   end
 
   it "has a creation date" do
-    expect(coin.created_at.class).to eq(ActiveSupport::TimeWithZone)
+    expect(@coin.created_at.class).to eq(ActiveSupport::TimeWithZone)
   end
 
   it "has a creation location" do
-    expect(coin.creation_location).to eq("Test location for coin")
+    expect(@coin.creation_location).to eq("Test location for coin")
   end
 
   it "has a code" do
-    expect(coin.code).to eq("1234")
+    expect(@coin.code).to eq("1234")
   end
 
   it "has a creator" do
-    expect(coin.creator).to eq(creator)
+    expect(@coin.creator).to eq(@creator)
   end
 
   it "has many moments" do
-    binding.pry 
-    expect(coin.moments.size).to eq(4)
+    expect(@coin.moments.size).to eq(4)
   end
 
   it "has many givers through moments" do
-    expect(coin.givers.size).to eq(4)
+    expect(@coin.givers.size).to eq(4)
   end
 
   it "has many receivers through moments" do
-    expect(coin.receivers.size).to eq(4)
+    expect(@coin.receivers.size).to eq(4)
   end
 
 end
