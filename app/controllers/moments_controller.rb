4766c86792a3f3206165e2984812830fa187ea92
Parker Lawrence <parker.alford@gmail.com>
2015-07-06 17:34:07 -0400

    complete basic moment form

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -1,2 +1,14 @@
 class MomentsController < ApplicationController
+
+  def new
+    @moment = Moment.new()
+    @date_today = date_today
+  end
+
+  private
+
+  def date_today
+    Time.now.strftime('%D')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   resources :groups
-
   resources :moments
-
   resources :coins
-
   devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
