d2ea66d43ea584b1de89d92abe7eb9e86230fbad
Parker Lawrence <parker.alford@gmail.com>
2015-07-14 19:55:18 -0400

    update coin show to fit theme

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,21 +1,21 @@
 class CoinsController < ApplicationController
 
   def lookup
-
-  end 
+  end
 
   def show
-    if params[:code] 
+    if params[:code]
       @coin = Coin.find_by(code: params[:code])
     else
       @coin = Coin.find_by(code: params[:id])
-    end 
-    
-    @moments = @coin.moments
+    end
+
+    @moments = @coin.moments if @coin
+
     respond_to do |format|
-      format.json {render json: {result: true}}
+      format.json {render json: {result: !!@coin}}
       format.html {}
-    end 
+    end
 
   end
 
