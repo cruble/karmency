255e87228f2e59903dc8b4e90472bb24a432049a
Parker Lawrence <parker.alford@gmail.com>
2015-07-20 23:35:46 -0400

    cookify coin creation for facebook login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+
 end
